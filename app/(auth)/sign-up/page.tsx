70f492b1d074f69fa5d5a39ed48377d7602806e6
briansastre-ops <briansastre158@gmail.com>
2025-02-17 23:27:34 -0300

    working in Auth

---
+++ app/(auth)/sign-up/page.tsx
@@ -0,0 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import AuthForm from '@/components/AuthForm'
+const SignUp = () => <AuthForm  type= "sign-up"/>
+export default SignUp
---
+++ components/AuthForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type FormType= "sign-in" | "sign-up" ;
+
+const authForm = ({type :{ type : FormType }}) => {
+  return (
+    <div>authForm</div>
+  )
+}
+
+export default authForm
