acee0ebb2d74532a47998e73580c0120cbdfa9a5
briansastre-ops <briansastre158@gmail.com>
2025-02-20 08:46:50 -0300

    ...

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,11 +1,66 @@
-import React from 'react'
+"use client"
 
-type FormType= "sign-in" | "sign-up" ;
+import { z } from "zod"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
 
-const authForm = ({type :{ type : FormType }}) => {
+const formSchema = z.object({
+  username: z.string().min(2).max(50),
+})
+
+type FormType = "sign-in" | "sign-up";
+
+interface AuthFormProps {
+  type: FormType;
+}
+
+const AuthForm: React.FC<AuthFormProps> = ({ type }) => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: "",
+    },
+  })
+
+  // 2. Define a submit handler.
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
+
   return (
-    <div>authForm</div>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="username"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Username</FormLabel>
+              <FormControl>
+                <Input placeholder="shadcn" {...field} />
+              </FormControl>
+              <FormDescription>
+                This is your public display name.
+              </FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   )
 }
 
-export default authForm
+export default AuthForm
